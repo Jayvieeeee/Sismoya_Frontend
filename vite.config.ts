42c5d43d09e9f73789d81f3648c1bb3f9e3cb262
ann <dalmaciochrisha6@gmail.com>
2025-10-01 19:44:37 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@views': path.resolve(__dirname, './src/view'),
-      '@layout': path.resolve(__dirname, './src/Layout'),
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@views": path.resolve(__dirname, "./src/view"),
+      "@layout": path.resolve(__dirname, "./src/Layout"),
     },
   },
-})
+  base: "./",
+});
